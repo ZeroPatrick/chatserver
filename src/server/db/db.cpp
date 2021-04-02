6ca3e8a9ebc89732171cfc13382f6c6f6c23a0f7
“ZeroPatrick” <1617534886@qq.com>
2021-04-02 17:24:16 +0800

    normal

--- src/server/db/db.cpp
+++ src/server/db/db.cpp
@@ -2,7 +2,8 @@
 #include <muduo/base/Logging.h>
 
 // 数据库配置信息
-static string server = "127.0.0.1";
+//static string server = "127.0.0.1";
+static string server = "47.99.168.24";
 static string user = "root";
 static string password = "123456";
 static string dbname = "chat";
